00e905a15669ca86fb8a4b081665a4bbb58a4e90
Nazimcp <75988681+Nazimcp@users.noreply.github.com>
2021-01-15 18:48:34 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sleep 2
+figlet Cyber_Black
+sleep 6
+echo $blue." PLEASE FOLLOW ME ON INSTAGRAM AND FACEBOOK";
+sleep 4
+echo $Red." GENARATING WORDLIST FOR YOU";                                                                                            
+sleep 4
+
+echo "This is a test"
